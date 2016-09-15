c60e475f5762f3cf3bcd5ef056d2df745a6895d7
Kevin Klues <klueska@gmail.com>
2016-09-14 17:22:47 -0700

    Added helpers for maintaining files in a container runtime directory.
    Review: https://reviews.apache.org/r/52013

---
+++ src/slave/containerizer/mesos/paths.cpp
@@ -0,0 +1,122 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <stout/os.hpp>
+#include <stout/path.hpp>
+
+#include "slave/containerizer/mesos/paths.hpp"
+
+using std::string;
+
+namespace mesos {
+namespace internal {
+namespace slave {
+namespace containerizer {
+namespace paths {
+
+string buildPathForContainer(
+    const ContainerID& containerId,
+    const string& prefix)
+{
+  if (!containerId.has_parent()) {
+    return path::join(prefix, containerId.value());
+  } else {
+    return path::join(
+        buildPathForContainer(containerId.parent(), prefix),
+        prefix,
+        containerId.value());
+  }
+}
+
+
+string getRuntimePath(
+    const string& runtime_dir,
+    const ContainerID& containerId)
+{
+  return path::join(
+      runtime_dir,
+      buildPathForContainer(containerId, CONTAINER_DIRECTORY));
+}
+
+
+Result<pid_t> getContainerPid(
+    const string& runtime_dir,
+    const ContainerID& containerId)
+{
+  const string path = path::join(
+    getRuntimePath(runtime_dir, containerId), PID_FILE);
+
+  if (!os::exists(path)) {
+    // This is possible because we don't atomically create the
+    // directory and write the 'pid' file and thus we might
+    // terminate/restart after we've created the directory but
+    // before we've written the file.
+    return None();
+  }
+
+  Try<string> read = os::read(path);
+  if (read.isError()) {
+    return Error("Failed to recover pid of container: " + read.error());
+  }
+
+  Try<pid_t> pid = numify<pid_t>(read.get());
+  if (pid.isError()) {
+    return Error(
+        "Failed to numify pid '" + read.get() +
+        "' of container at '" + path + "': " + pid.error());
+  }
+
+  return pid.get();
+}
+
+
+Result<int> getContainerStatus(
+    const string& runtime_dir,
+    const ContainerID& containerId)
+{
+  const string path = path::join(
+    getRuntimePath(runtime_dir, containerId), STATUS_FILE);
+
+  if (!os::exists(path)) {
+    return None();
+  }
+
+  Try<string> read = os::read(path);
+  if (read.isError()) {
+    return Error("Unable to read status for container '" +
+                 containerId.value() + "' from checkpoint file '" +
+                 path + "': " + read.error());
+  }
+
+  if (read.get() != "") {
+    Try<int> containerStatus = numify<int>(read.get());
+    if (containerStatus.isError()) {
+      return Error("Unable to read status for container '" +
+                   containerId.value() + "' as integer from '" +
+                   path + "': " + read.error());
+    }
+
+    return containerStatus.get();
+  }
+
+  return None();
+}
+
+} // namespace paths {
+} // namespace containerizer {
+} // namespace slave {
+} // namespace internal {
+} // namespace mesos {
