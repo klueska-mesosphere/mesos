a6a3eb99fe221ed01e7fc275971a21577a256a06
Gilbert Song <songzihao1990@gmail.com>
2016-09-20 17:20:39 -0700

    Fixed build failure on clang with WEXITSTATUS (squash me).

--- src/slave/containerizer/mesos/containerizer.cpp
+++ src/slave/containerizer/mesos/containerizer.cpp
@@ -2135,8 +2135,9 @@ Future<Option<int>> MesosContainerizerProcess::reap(
       }
 
       if (WEXITSTATUS(status.get()) != EXIT_SUCCESS) {
-        return Failure("The 'init' process of the container exited with"
-                       " exit status: " + WEXITSTATUS(status.get()));
+        return Failure(
+            "The 'init' process of the container exited with "
+            "exit status: " + stringify(WEXITSTATUS(status.get())));
       }
 
       const string statusPath =
