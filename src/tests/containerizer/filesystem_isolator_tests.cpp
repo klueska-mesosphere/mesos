cd2f22f9ad1b61bdf266afb9146396a3097913ee
Gilbert Song <songzihao1990@gmail.com>
2016-09-21 22:39:30 -0700

    Fixed unit test 'ROOT_RecoverOrphanedPersistentVolume'.

--- src/tests/containerizer/filesystem_isolator_tests.cpp
+++ src/tests/containerizer/filesystem_isolator_tests.cpp
@@ -736,12 +736,6 @@ TEST_F(LinuxFilesystemIsolatorTest, ROOT_RecoverOrphanedPersistentVolume)
   Future<Nothing> _recover = FUTURE_DISPATCH(_, &Slave::_recover);
   AWAIT_READY_FOR(_recover, Seconds(60));
 
-  // Wait until the containerizer's recovery is done too.
-  // This is called once orphans are cleaned up.  But this future is not
-  // directly tied to the `Slave::_recover` future above.
-  _recover = FUTURE_DISPATCH(_, &MesosContainerizerProcess::___recover);
-  AWAIT_READY_FOR(_recover, Seconds(60));
-
   Try<fs::MountInfoTable> table = fs::MountInfoTable::read();
   ASSERT_SOME(table);
 
